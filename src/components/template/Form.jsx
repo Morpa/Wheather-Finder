67ef1dcb69a519b65f38d1ec453414ea650c0e04
Morpa <andremorpa@gmail.com>
2018-12-18 09:21:27 +0000

    Adicionado a funcionalidade de realizar a busca clicando enter

--- src/components/template/Form.jsx
+++ src/components/template/Form.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './Form.css'
 
-export default props => (
-    <div role='form' className="col-sm-12 pt-4">
-        <input id="city" type="text"
-            placeholder="Cidade..."
-            onChange={props.handleChange}
-            value={props.city}></input>
-        <button onClick={props.handleCall}>Buscar</button>
-    </div>
-)
+export default props => {
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey = props.handleCall()
+        }
+    }
+
+    return (
+        <div role='form' className="col-sm-12 pt-4">
+            <input id="city" type="text"
+                placeholder="Cidade..."
+                onChange={props.handleChange}
+                onKeyUp={keyHandler}
+                value={props.city}></input>
+            <button onClick={props.handleCall}>Buscar</button>
+        </div>
+    )
+}
